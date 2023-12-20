b292ec3144d2132b8e09597d2f0d02da68e4d238
nagatomo0929 <132539335+nagatomo0929@users.noreply.github.com>
2023-12-20 13:24:12 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,9 +1,9 @@
-const date = new Date();
+const date = new Date([2023,8,22]);
 
 const[year,month,day] =[
     date.getFullYear(),
-    date.getMonth(),
-    date.getDay(),
+    date.getMonth() +1,
+    date.getDate(),
 ];
 
 console.log(year + '年' + month + '月' + day + '日');
