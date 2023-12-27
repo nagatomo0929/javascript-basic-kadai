6e094a3bb3c93533d4d9167366620aa94b75e64f
nagatomo0929 <132539335+nagatomo0929@users.noreply.github.com>
2023-12-27 11:37:32 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+  const text = document.getElementByld('text');
+  text.textContent = 'ボタンをクリックしました';
+});
